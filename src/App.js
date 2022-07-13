0e9289c6ffc227d2f767940cbf1f9170d0bf5609
binalbodar <binalbodar23@gmail.com>
2022-07-12 19:51:09 -0700

    13/7/22parsist

--- src/App.js
+++ src/App.js
@@ -6,20 +6,23 @@ import Medicine from "./Container/Medicines/Medicine";
 import { Provider } from 'react-redux'
 import { configurStore } from "./Redux/Store";
 import Counter from "./Redux/Reducer/Counter";
+import { PersistGate } from 'redux-persist/integration/react'
 
 function App() {
-  const store = configurStore();
+  const {store, persistor} = configurStore();
   return (
     <>
       <Provider store={store}>
-        <Layout>
-          <Switch>
-            <Route exact path={"/medicine"} component={Medicine} />
-            <Route exact path={"/medicineformik"} component={Medicineformik} />
-            <Route exact path={"/doctor"} component={Doctor} />
-            <Route exact path={"/counter"} component={Counter} />
-          </Switch>
-        </Layout>
+        <PersistGate loading={null} persistor={persistor}>
+          <Layout>
+            <Switch>
+              <Route exact path={"/medicine"} component={Medicine} />
+              <Route exact path={"/medicineformik"} component={Medicineformik} />
+              <Route exact path={"/doctor"} component={Doctor} />
+              <Route exact path={"/counter"} component={Counter} />
+            </Switch>
+          </Layout>
+        </PersistGate>
       </Provider>
     </>
   );
